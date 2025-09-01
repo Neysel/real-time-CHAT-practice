79284d21735e1eb74f57478cd0a823bd2dfbb21b
Neysel <neyseltheblack@gmail.com>
2025-09-02 00:34:04 +0400

    test beginning

---
+++ folder_name/src/components/AppRouter.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import {Route, Routes, Navigate} from 'react-router-dom'
+import { privateRouotes, publicRoutes } from '../routes';
+import { CHAT_ROUTE, LOGIN_ROUTE } from '../utils/consts';
+import Login from './Login';
+import Chat from './Chat';
+
+
+const AppRouter = () => {
+    const user = false
+
+    return  user ?  ( 
+       
+        <Routes>
+            {privateRouotes.map(({path, Component}) => 
+                <Route path={path} element={Component}/>
+            )}
+            <Route path='*' element={<Chat/>} />
+        </Routes>
+        
+     ) : (
+        <Routes>
+            {publicRoutes.map(({path, Component}) => 
+                <Route path={path} element={Component}/>
+            )}
+            <Route path='*' element={<Login/>} />
+       </Routes>)
+}
+ 
+export default AppRouter;
