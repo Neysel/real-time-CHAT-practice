37f9b66ff9c24073c19ef83b24433fceeddae9e9
Neysel <neyseltheblack@gmail.com>
2025-11-04 04:03:11 +0400

    renamed folder to firebase_google_chat

---
+++ firebase_google_chat/src/components/Chat.js
@@ -0,0 +1,93 @@
+import React, { useContext, useState } from 'react';
+import Grid from '@mui/material/Grid';
+import {useAuthState} from "react-firebase-hooks/auth";
+import { useCollectionData } from "react-firebase-hooks/firestore"
+import { ContextFire } from '..';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Loader from './Loader';
+import { addDoc, collection, serverTimestamp, query, orderBy } from 'firebase/firestore'; // Import firestore functions
+import 'firebase/compat/firestore'; // For compatibility library
+import 'firebase/compat/auth'; // For compatibility library
+import Avatar from '@mui/material/Avatar';
+ 
+
+const Chat = () => {
+      const {auth, firestore} = useContext(ContextFire)
+    const [user, loading, error] = useAuthState(auth)
+    const [value, setValue] = useState('')
+    const [messages, messagesLoading] = useCollectionData(
+        //   firestore.collection('message').orderBy('createdAt')
+        query(collection(firestore, 'messages'), orderBy('createdAt'))
+    )
+
+    const sendMessage = async () => {
+        // firestore.collection(firestore, 'message').add({
+        const docRef = await addDoc(collection(firestore, 'messages'), {
+            uid: user.uid, 
+            displayName: user.displayName,
+            photoUrl: user.photoURL, 
+            text: value, 
+            // createdAt: firebase.firestore.FieldValue.serverTimestamp()
+            createdAt: serverTimestamp()
+                })
+                setValue('')
+
+    }
+if (loading || messagesLoading) {
+    return <Loader/>
+}
+
+    return ( 
+        <Container>
+           <Grid container
+                style={{height: window.innerHeight - 50}}
+                alignItems={"center"}
+                justifyContent={"center"}
+            >
+                <div style={{width: '80%', height: '70vh', border: '1px solid gray', overflowY: 'auto'}}> 
+                    {messages && messages.map(msg => (
+                        <div style={{
+                            margin: 10,
+                            // border: user.uid === messages.uid ? '2px solid green' : '2px dashed red', 
+                          
+                           width: '95%',
+                           padding: 5
+                        }}>
+                            <Grid 
+                            style={{
+                                width: 'fit-content',
+                                 marginLeft: user.uid == msg.uid ? 'auto' : '10px', 
+                            }}>
+                                  <Avatar src={msg.photoUrl}/>
+                            <div>{msg.displayName}</div>
+                            
+                            </Grid>
+                            <div 
+                            style={{ width: 'fit-content',marginLeft: user.uid == msg.uid ? 'auto' : '10px', }}
+                            >{msg.text}</div>
+                        </div>
+                    ))}
+                </div>
+                <Grid
+                    container
+                    direction={"column"}
+                    alignItems={'flex-end'}
+                    style={{width: '80%'}}
+                    >
+                        <TextField 
+                        fullWidth
+                        maxRows ={2}
+                        value={value}
+                        onChange={e => setValue(e.target.value)}
+                        />
+                        <Button onClick={sendMessage}>SEND</Button>
+                </Grid>
+               
+                </Grid>
+        </Container>
+     );
+    }
+ 
+export default Chat;
---
+++ firebase_google_chat/src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Grid from '@mui/material/Grid';
+import Button from '@mui/material/Button';
+import {useAuthState} from "react-firebase-hooks/auth"
+import { ContextFire } from '..';
+
+const Navbar = () => {
+    // const user = false
+    const {auth} = useContext(ContextFire)
+    const [user] = useAuthState(auth)
+
+
+    return ( 
+        
+        <AppBar color={"secondary"} position="static">
+  <Toolbar variant="dense" justify={"flex-end"} style={{justifyContent: "flex-end"}}>
+        <Grid container  style={{justifyContent: "flex-end"}}>
+            {user ? 
+            <Button onClick={() => auth.signOut()} variant={"outlined"} style={{color: "black", borderColor: "black" }} >Logout</Button>
+            :     
+            <Button variant={"outlined"} style={{color: "black", borderColor: "black", marginRight: "5px"}} >Login</Button>
+        }
+          
+            
+        </Grid>
+  </Toolbar>
+</AppBar>
+     );
+}
+ 
+export default Navbar;
